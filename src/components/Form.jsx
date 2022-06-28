2680d5149c1f169f957e34adcc9c276a234ed89a
Shahed-BD71 <mdshahedbd71@gmail.com>
2022-06-29 03:23:36 +0600

    fix ui

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -52,7 +52,7 @@ export default function ProfitForm() {
               <h2 className="text-3xl font-semibold">Profit</h2>
               <h2 className="text-3xl font-semibold text-green-400">Calculator</h2>
             </div>
-            <div className="md:flex max-w-screen p-12">
+            <div className="md:flex max-w-screen py-12">
               <Hero />
               <div className="w-full p-4 md:px-16 px-8 py-8">
                 <form>
